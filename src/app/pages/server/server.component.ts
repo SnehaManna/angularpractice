6ef5fb9514ee582a8b7f8a623416bcd003b139c7
SnehaManna <snehamanna62@gmail.com>
2023-03-18 19:54:12 +0530

    debugging

--- src/app/pages/server/server.component.ts
+++ src/app/pages/server/server.component.ts
@@ -9,16 +9,27 @@ export class ServerComponent implements OnInit {
 serverId:number=10;
 ServerStatus="offline";
 tabcontent=false;
+displayText=false;
+log:number[]=[]
 getServerStatus(){
 return(
   this.ServerStatus
 )
 }
-  constructor() { }
+  constructor() {
+    this.ServerStatus=Math.random()>0.5?"online":"offline";
+   }
 
   ngOnInit(): void {
   }
   tabClick(){
 this.tabcontent=!this.tabcontent;
+  }
+  getColor(){
+   return this.ServerStatus==="online"?"green":"red"
+  }
+  getDisplay(){
+this.displayText=!this.displayText;
+this.log.push(this.log.length +1)
   }
 }
